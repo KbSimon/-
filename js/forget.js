32ed3d2d1bc0e21aeeddc9ff3a429721dd995c94
Wang Yaqi <857102829@qq.com>
2017-12-12 14:45:44 +0800

    忘记交易密码

--- js/forget.js
+++ js/forget.js
@@ -5,12 +5,3 @@ $(".forget_ul01>li").click(function () {
 $(".forget_ul02>li").click(function () {
     $(this).attr("id", "active").siblings().removeAttr("id", "active");
 })
-
-// 显示注册第一步
-// $(".next").click(function () {
-//     $(".forget_part01").hide();
-//     $(".forget_part02").show();
-//     $(".forget_top_img").attr("src", "../../images/forget/forget_02.png");
-// });
-
-
