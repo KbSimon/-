7256b030c4b9b3a570de319d383913eec9cec099
Wang Yaqi <857102829@qq.com>
2018-01-25 20:32:06 +0800

    关于我们

--- js/head.js
+++ js/head.js
@@ -1,8 +1,8 @@
 $(window).scroll(function () {
-    if($(window).scrollTop() > 100){
-        $("header").css("position","fixed");
-    } else if($(window).scrollTop() < 100){
-        $("header").css("position","static");
+    if($(window).scrollTop() > 60){
+        $("header").css({"position":"fixed","top":"0"});
+    } else if($(window).scrollTop() < 60){
+        $("header").css({"position":"static"});
     }
 });
 
